90d38b5964b22d429170b734a2b2e9dda1fe7b1e
Mahrjose <mahrjose.dev@gmail.com>
2022-10-29 02:49:21 +0600

    Add Linux `Post-Installation.sh` script

---
+++ Linux/Post-Installation.sh
@@ -0,0 +1,10 @@
+#! /usr/bin/bash
+
+# Author: Mirza Mahrab Hossain
+# Email : mahrjose.dev@gmail.com
+# Github: github.com/Mahrjose/Scripts
+# ========================================== #
+
+# Update System
+sudo apt update 
+sudo apt upgrade -y
